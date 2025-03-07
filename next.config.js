4b9898a9410aeccdae6ca6c208c810a8ef600b34
Jan Pavlovsky <honza@woodlo.com>
2025-03-07 22:56:09 +0100

    Updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    output: 'export', // Povolení statického exportu
-    images: {
-      unoptimized: true, // Obrázky nebudou optimalizované
-    },
-    basePath: "/hometown-open-2025", // Důležité pro GitHub Pages
-  };
-  
